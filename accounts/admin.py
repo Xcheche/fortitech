146e3912e0940e4fe258e7a75846c44a9ed47bac
xcheche <chekwubechukwu23@gmail.com>
2025-08-08 12:05:23 +0100

    auth
    auth done

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import *
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_active', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('is_active', 'is_staff')
+    ordering = ('-created_at','updated_at',)
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(PendingUser)
--- common/tasks.py
+++ common/tasks.py
@@ -1,12 +1,32 @@
+# from django.core.mail import EmailMultiAlternatives
+# from django.template.loader import get_template
+
+
+# def send_email(subject: str, email_to: list[str], html_template, context):
+#     msg = EmailMultiAlternatives(
+#         subject=subject, from_email="noreply@example.com", to=email_to
+#     )
+#     html_template = get_template(html_template)
+#     html_content = html_template.render(context)
+#     msg.attach_alternative(html_content, "text/html")
+#     msg.send(fail_silently=False)
+
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
-
+from django.utils.html import strip_tags
 
 def send_email(subject: str, email_to: list[str], html_template, context):
-    msg = EmailMultiAlternatives(
-        subject=subject, from_email="noreply@example.com", to=email_to
-    )
     html_template = get_template(html_template)
     html_content = html_template.render(context)
+    
+    # Create a plain text version from the HTML content
+    text_content = strip_tags(html_content)
+
+    msg = EmailMultiAlternatives(
+        subject=subject, 
+        body=text_content,  # Set the plain text as the main body
+        from_email="noreply@example.com", 
+        to=email_to
+    )
     msg.attach_alternative(html_content, "text/html")
     msg.send(fail_silently=False)

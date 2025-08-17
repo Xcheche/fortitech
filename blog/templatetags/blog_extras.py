ac2fd75f53046d2145f409252792b52ff477ceb9
xcheche <chekwubechukwu23@gmail.com>
2025-08-17 16:29:04 +0100

    deployment

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -8,5 +8,6 @@ register = template.Library()
 def nice_name(email):
     """
     Converts 'john.doe@example.com' → 'John Doe'
+    
     """
     return email.split("@")[0].replace(".", " ").replace("_", " ").title()

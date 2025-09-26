02d8d396da8eaa9a41acf30e5c40e6b6eca0d352
xcheche <chekwubechukwu23@gmail.com>
2025-09-26 13:31:54 +0100

    new feature

--- config/settings/celery.py
+++ config/settings/celery.py
@@ -2,14 +2,14 @@ from decouple import config
 from .base import *  # noqa: F403
 import os
 
-CELERY_BROKER_URL = config("CELERY_BROKER_URL", default="redis://localhost:6379/0")
-CELERY_ACCEPT_CONTENT = ["json"]
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_BACKEND = config(
-    "CELERY_RESULT_BACKEND", default="redis://localhost:6379/0"
-)
-CELERY_TIMEZONE = config("CELERY_TIMEZONE", default="UTC")
+# CELERY_BROKER_URL = config("CELERY_BROKER_URL", default="redis://localhost:6379/0")
+# CELERY_ACCEPT_CONTENT = ["json"]
+# CELERY_TASK_SERIALIZER = "json"
+# CELERY_RESULT_BACKEND = config(
+#     "CELERY_RESULT_BACKEND", default="redis://localhost:6379/0"
+# )
+# CELERY_TIMEZONE = config("CELERY_TIMEZONE", default="UTC")
 
 
-# Debugging
-print("CELERY_BROKER_URL:", CELERY_BROKER_URL)
+# # Debugging
+# print("CELERY_BROKER_URL:", CELERY_BROKER_URL)
--- config/settings/local.py
+++ config/settings/local.py
@@ -1,9 +1,11 @@
-from .base import *
+from .base import *  # noqa: F403
 from decouple import config, Csv
+import psycopg2  # noqa: F401
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default="localhost,127.0.0.1")
 
-# Use SQLite for local development to simplify if you want
+#=====NOt in use now =====
+#Use SQLite for local development to simplify if you want
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.sqlite3",
@@ -11,17 +13,17 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default="localhost,127.0.0.1
 #     }
 # }
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fortitech_local',
-        'USER': 'cheche',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        # No password needed since PostgreSQL is configured with 'trust' authentication
-        'PASSWORD': '',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",  # Supabase default DB name
+        "USER": "postgres.cmxrwwzurbtrrjvhwhfd",  # your actual user
+        "PASSWORD": "fortitech2025",
+        "HOST": "aws-1-eu-west-2.pooler.supabase.com",
+        "PORT": "5432",  # 👈 switch to 5432
+        "OPTIONS": {
+            "sslmode": "require",  # 👈 force SSL
+        },
     }
 }
-
-
-print(DATABASES)
